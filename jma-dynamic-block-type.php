5f260fde75dc41070377a8cba654405b6943bb38
johnnya23 <johnantonacci23@gmail.com>
2018-11-27 18:08:35 -0700

    setup structure

---
+++ jma-dynamic-block-type.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: JMA Carousel
+Description: Uses slick jquery plugin to convert minipost grid to carousel
+Version: 1.0
+Author: John Antonacci
+License:
+http://kenwheeler.github.io/slick/
+*/
